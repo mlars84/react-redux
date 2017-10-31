292081d094306b5fc440252c7240013de9cf9286
Matt Larson <matt.a.larson@gmail.com>
2017-10-31 15:36:50 -0500

    repo for udemy course react-redux

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyDL0r5DA1SxTB1CH89S2B3JAjfZTnJAdZg'
+
+const App = function() {
+  return <div>hi</div>
+}
+
+ReactDOM.render(
+  <App />, 
+  document.querySelector('.root')
+);
