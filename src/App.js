4a0844345dd4e873dd4c08e7d7be7d4abcb1e99a
Matt Larson <matt.a.larson@gmail.com>
2017-10-31 18:04:47 -0500

    refactored App component out of index.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import SearchBar from './components/search_bar'
+import VideoDetail from './components/video_detail'
+import VideoListItem from './components/video_list_item'
+import VideoList from './components/video_list'
+
+const API_KEY = 'AIzaSyDL0r5DA1SxTB1CH89S2B3JAjfZTnJAdZg'
+
+class App extends Component {
+  render() {
+    // console.log('App props:', this.props)
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail />
+        <VideoListItem />
+        <VideoList />
+      </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import SearchBar from './components/search_bar'
-import VideoDetail from './components/video_detail'
-import VideoListItem from './components/video_list_item'
-import VideoList from './components/video_list'
-
-const API_KEY = 'AIzaSyDL0r5DA1SxTB1CH89S2B3JAjfZTnJAdZg'
-
-class App extends Component {
-  render() {
-    // console.log('App props:', this.props)
-    return (
-      <div>
-        <SearchBar />
-        <VideoDetail />
-        <VideoListItem />
-        <VideoList />
-      </div>
-    )
-  }
-}
+import App from './App'
 
 ReactDOM.render(
   <App />, 
